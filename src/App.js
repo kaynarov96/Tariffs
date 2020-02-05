f280d6e83454a6b13fdb85548136890535346dc8
AzamatKaynarov <kaynarov2909@mail.ru>
2020-02-05 18:20:48 +0500

    welcome massage changed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
         <div>
             <div className="title">
             <span>Будь на волне даже при нуле</span>
+            <h1>hello from react</h1>
             </div>
             <h1>Тарифы</h1>
             <TariffsList/>          

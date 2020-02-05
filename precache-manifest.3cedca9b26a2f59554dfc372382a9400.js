1a26cb38a245980616819f3d92ca605e93ad6d6a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-05 13:15:05 +0000

    Deploy kaynarov96/Tariffs to github.com/kaynarov96/Tariffs.git:gh-pages

---
+++ precache-manifest.3cedca9b26a2f59554dfc372382a9400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff2ffb19ea54d4e164da69270be0a8c",
+    "url": "/Tariffs/index.html"
+  },
+  {
+    "revision": "326994f19b3c7e27b5bb",
+    "url": "/Tariffs/static/css/main.9888f065.chunk.css"
+  },
+  {
+    "revision": "b5bc3d37c73adb2943c0",
+    "url": "/Tariffs/static/js/2.c643a220.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Tariffs/static/js/2.c643a220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "326994f19b3c7e27b5bb",
+    "url": "/Tariffs/static/js/main.2c3f5c8a.chunk.js"
+  },
+  {
+    "revision": "7996b5febe52edbe4185",
+    "url": "/Tariffs/static/js/runtime-main.c402029b.js"
+  }
+]);
